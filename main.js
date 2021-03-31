cc900fab3beb899b6ce7ecea66dd11d4f3c6a24f
lcampi <lcampi@indec.gob.ar>
2021-03-31 10:20:39 -0300

    feat(src): make main.js base

--- main.js
+++ main.js
@@ -1,56 +1,64 @@
 // SET SCREEN
 const screen = document.getElementById('entry');
-const smallScreen = document.getElementById('smallEntry')
+const smallScreen = document.getElementById('smallEntry');
 
 // INITIALIZE VALUE VARIABLES
-let currentValue = '', oldValues = [], oldValuesConcatenated = '', number = '', numbers = [0], result = 0;
+let number = 0, current = [], operationNumbers = [], operatedNumber = 0;
 
 // CREATE CALCULATOR FUNCTIONALITY
-
 const getValue = value => {
-    if(currentValue === '') {
-        currentValue = value;
-        screen.innerHTML = currentValue
-
-    } else {
-        oldValues.push(currentValue);
-        currentValue = value;
-        for (let i = 0; i < oldValues.length; i++) {
-            oldValuesConcatenated = oldValues.join('')
-            number = oldValuesConcatenated + currentValue;
-            screen.innerHTML = number;
-        }
-    }   
+    current.push(value); 
+    number = current.join('');
+    screen.innerHTML = number;
 }
 
 const operations = operator => {
-    currentValue = '', oldValues = [];
-    screen.innerHTML = '0';
-    if (number !== '') {
-        numbers.push(parseFloat(number));
-        number = '';
-    }
-    switch(operator) {
-        case '+':
-        for(let i = 1; i < numbers.length; i++) {
-            result += numbers[i];
-            numbers[i] = 0;
+    switch(operator){
+        // ADD OPERATIONS FUNCTIONALITY
+        //operationNumbers have te numbers we were entering before.
+        //operatedNumber will recive the corresponding operations with numbers recived from operationNumbers
+        case '+': 
+        operationNumbers.push(number);
+        number = 0, current = [];
+        if (operationNumbers.length === 1) {
+        smallScreen.innerHTML = operationNumbers[0] + ' +';
         }
-        
+        break;
+
+        case '-':
+            operationNumbers.push(number);
+            number = 0, current = [];
+            if (operationNumbers.length === 1) {
+            smallScreen.innerHTML = operationNumbers[0] + ' -';
+            }
+        break;
+
+        case '*':
+            operationNumbers.push(number);
+            number = 0, current = [];
+            if (operationNumbers.length === 1) {
+            smallScreen.innerHTML = operationNumbers[0] + ' *';
+            }
+        break;
+
+        case '/':
+            operationNumbers.push(number);
+            number = 0, current = [];
+            if (operationNumbers.length === 1) {
+            smallScreen.innerHTML = operationNumbers[0] + ' /';
+            }
+        break;
     }
 }
 
 const clearScreen = () => {
-    currentValue = '', oldValues = [];
+number = 0, current = [], operationNumbers = [];
 screen.innerHTML = '0';
 smallScreen.innerHTML = '0'
 }
 
 const equal = () => {
-currentValue = '', oldValues = [], oldValuesConcatenated = '', number = '', numbers = [0];
-console.log(result);
-screen.innerHTML = result;
-result = 0;
+
 }
 
 
